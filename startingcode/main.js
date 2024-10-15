14d108fbf7531b3283207bc8a5fbf3124d562a48
Jovica Kuzmanovic <jovica04@outlook.com>
2024-10-15 14:01:01 -0700

    first commit

---
+++ startingcode/main.js
@@ -0,0 +1,6 @@
+const path = require("path");
+
+const IOhandler = require("./IOhandler");
+const zipFilePath = path.join(__dirname, "myfile.zip");
+const pathUnzipped = path.join(__dirname, "unzipped");
+const pathProcessed = path.join(__dirname, "grayscaled");
